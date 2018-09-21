58cf5876555a2e981f3ff2eda61092e1fad03482
Oscar Tellez <oscartzgz@gmail.com>
2018-09-20 19:20:09 -0500

    routes for api response

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, format: :json do
+    resources :employees
+  end
+
   get 'home/login'
-
   get 'home/index'
-
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
